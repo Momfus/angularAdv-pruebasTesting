62d40bcaaf26bffa8a1954a32ff9c7ec56ac96b0
Momfus <momfus@outlook.com>
2023-01-06 17:36:15 -0300

    probando claes - clase 278

---
+++ src/app/basicas/clases/clase.spec.ts
@@ -0,0 +1,50 @@
+import { Jugador } from './clase';
+
+let jugador = new Jugador();
+
+beforeAll( () => {
+  // console.log('beforeAll');
+});
+
+beforeEach( () => {
+  // console.log('beforeEach');
+  // jugador.hp = 100;
+  jugador = new Jugador(); // Inicializarlo de nuevo
+});
+
+afterAll( () => {
+  // console.log('afterAll');
+});
+
+afterEach( () => {
+  // console.log('afterEach');
+  // jugador.hp = 100;
+});
+
+
+describe( 'Pruebas de clase', () => {
+
+
+  it( 'Debe de retornar 80 de hp, si recibe 20 de daño', () => {
+
+    const res = jugador.recibeDanio(20);
+
+    expect( res ).toBe(80);
+
+  });
+
+  it( 'Debe de retornar 50 de hp, si recibe 50 de daño', () => {
+
+    const res = jugador.recibeDanio(50);
+    expect( res ).toBe(50);
+
+  });
+
+  it( 'Debe de retornar 0 de hp, si recibe 100 de daño', () => {
+
+    const res = jugador.recibeDanio(100);
+    expect( res ).toBe(0);
+
+  });
+
+});
