22c13321e588810845c30ac28f77cf03b91b34f7
Momfus <momfus@outlook.com>
2023-01-06 13:09:05 -0300

    Prueba básica de strings - clase 272

---
+++ src/app/basicas/string/string.spec.ts
@@ -0,0 +1,26 @@
+import { mensaje } from './string';
+
+// La pruebas que se tienen con una "x" al inicio se ignoran
+// Importante que se llame como el archivo que se hace prueba con el agregado de "specs"
+// En la pantalla de pruebas, si se coloca "debug", en la consola saldrá los mensajes que tengan en console.log las pruebas
+
+// Para agrupar pruebas
+//describe('Pruebas de Strings');
+
+// Una prueba en concreto
+// it('Debe regresar un string');
+// it('Debe contender un nombre');
+
+
+describe( 'Pruebas de strings', () => {
+
+  it( 'Debe de regresar un string', () => {
+
+    const res = mensaje('Momfus');
+
+    // expect sirve para saber lo que se "espera" ser exitoso, pueden ser varios expects
+    expect( (typeof res) ).toBe('string');
+
+  });
+
+});
